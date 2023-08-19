d6dd56928ef60a6666505321bb0bd5ab80bb3fc8
suzutan <6679870+suzutan@users.noreply.github.com>
2023-08-19 07:24:24 +0000

    node_count, flavorを定義

--- basic-gke/gcp-cluster.tf
+++ basic-gke/gcp-cluster.tf
@@ -2,8 +2,11 @@
 module "labo-gke" {
   source = "../modules/gcp-basic-gke"
 
-  project_id         = "harvestasya-344919"
-  region             = "asia-northeast1"
-  gke_cluster_name   = "labo-basic-gke"
-  min_master_version = "1.27.2-gke.1200"
+  project_id               = "harvestasya-344919"
+  region                   = "asia-northeast1"
+  gke_cluster_name         = "labo-basic-gke"
+  min_master_version       = "1.27.2-gke.1200"
+  gke_node_count           = 2
+  gke_node_machine_type    = "e2-standard-4"
+  gke_node_autoscaling_max = 3
 }

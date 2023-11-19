471a5900e275b7b004b8d2f71d1e242a9894a8e0
suzutan <6679870+suzutan@users.noreply.github.com>
2023-11-19 15:18:04 +0900

    update

--- basic-gke/gcp-cluster.tf
+++ basic-gke/gcp-cluster.tf
@@ -5,7 +5,7 @@ module "labo-gke" {
   project_id               = "harvestasya-344919"
   region                   = "asia-northeast1"
   gke_cluster_name         = "labo-basic-gke"
-  min_master_version       = "1.27.2-gke.1200"
+  min_master_version       = "1.27.5-gke.200"
   gke_node_count           = 2
   gke_node_machine_type    = "e2-standard-4"
   gke_node_autoscaling_max = 3
